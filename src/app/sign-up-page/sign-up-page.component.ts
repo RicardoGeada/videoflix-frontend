706c8910ab776b151129cce1b3942872e7b0ea8f
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-09-05 00:10:36 +0200

    passwordsMismatch validator

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { passwordsMatchValidator } from './validators/passwordsMatch.validator';
+
 @Component({
   selector: 'app-sign-up-page',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ReactiveFormsModule],
   templateUrl: './sign-up-page.component.html',
   styleUrl: './sign-up-page.component.scss'
 })
@@ -14,12 +18,37 @@ export class SignUpPageComponent {
   passwordImageSource = { value: "visibility.svg" };
   confirmPasswordImageSource = { value: "visibility.svg" };
 
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    }, {
+      validator: passwordsMatchValidator('password', 'confirmPassword')
+    } as AbstractControlOptions);
+  }
+
+
   togglePasswordVisibility(passwordInput : HTMLInputElement) {
     let imgSrc = passwordInput.id == "password" ? this.passwordImageSource : this.confirmPasswordImageSource;
     passwordInput.type == "password" ? passwordInput.type = "text" : passwordInput.type = "password";
     passwordInput.type == "password" ?  imgSrc.value = "visibility.svg" :  imgSrc.value = "visibility-off.svg";
   }
 
-  
+
+  passwordsMismatch() {
+    const confirmPasswordControl = this.form.get('confirmPassword');
+    if (confirmPasswordControl) {
+      return confirmPasswordControl.hasError('passwordsMismatch') && confirmPasswordControl.touched
+    } else {
+      return false;
+    }
+  }
+
+  onSubmit() {
+
+  }
 
 }
