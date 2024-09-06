7d7a61a48db1eec731be016e7b7ccb1aca577a79
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-09-06 14:57:08 +0200

    basic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent},
     { path: 'login', component: LoginPageComponent},
     { path: 'signup', component: SignUpPageComponent},
+    { path: 'reset-password', component: ResetPasswordPageComponent},
 ];
