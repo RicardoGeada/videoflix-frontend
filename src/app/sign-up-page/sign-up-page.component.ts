6efe5c604e1e0999a1310bd9b36ebb796fc2ba0c
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-09-06 14:24:56 +0200

    form validation on submit

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -2,44 +2,73 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
 
-import { ReactiveFormsModule } from '@angular/forms';
-import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { passwordsMatchValidator } from './validators/passwordsMatch.validator';
 import { FormInputComponent } from '../shared/components/form-input/form-input.component';
 
 @Component({
   selector: 'app-sign-up-page',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, ReactiveFormsModule, FormInputComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ReactiveFormsModule,
+    FormInputComponent,
+  ],
   templateUrl: './sign-up-page.component.html',
-  styleUrl: './sign-up-page.component.scss'
+  styleUrl: './sign-up-page.component.scss',
 })
 export class SignUpPageComponent {
-
   form: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-      confirmPassword: ['', [Validators.required]]
-    }, {
-      validator: passwordsMatchValidator('password', 'confirmPassword')
-    } as AbstractControlOptions);
+    this.form = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validator: passwordsMatchValidator('password', 'confirmPassword'),
+      } as AbstractControlOptions
+    );
   }
-
 
   passwordsMismatch() {
     const confirmPasswordControl = this.form.get('confirmPassword');
     if (confirmPasswordControl) {
-      return confirmPasswordControl.hasError('passwordsMismatch') && confirmPasswordControl.touched
+      return (
+        confirmPasswordControl.hasError('passwordsMismatch') &&
+        confirmPasswordControl.touched
+      );
     } else {
       return false;
     }
   }
 
   onSubmit() {
-
+    if (this.form.valid) {
+      // submit
+    } else {
+      this.validateAllFormFields(this.form);
+    }
   }
 
+  
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    });
+  }
 }
