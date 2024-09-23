43d5c687faed9033d684dd18695d7bb1136b97c7
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-09-23 13:59:51 +0200

    added progress bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
 import { ForgotPasswordPageComponent } from './forgot-password-page/forgot-password-page.component';
 import { BrowsePageComponent } from './browse-page/browse-page.component';
+import { VideoplayerPageComponent } from './videoplayer-page/videoplayer-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent},
@@ -13,4 +14,5 @@ export const routes: Routes = [
     { path: 'reset-password', component: ResetPasswordPageComponent},
     { path: 'forgot-password', component: ForgotPasswordPageComponent},
     { path: 'browse', component: BrowsePageComponent},
+    { path: 'play', component: VideoplayerPageComponent},
 ];
--- src/app/videoplayer-page/videoplayer-page.component.ts
+++ src/app/videoplayer-page/videoplayer-page.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-videoplayer-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './videoplayer-page.component.html',
   styleUrl: './videoplayer-page.component.scss'
 })
 export class VideoplayerPageComponent {
+
+  @ViewChild('video') video! : ElementRef<HTMLVideoElement>;
+
+  duration: number = 0;
+  currentTime : number = 0;
+  volume: number = 1;
+
+  ngAfterViewInit() {
+    const video = this.video.nativeElement;
+    video.volume = this.volume;
+    video.addEventListener('loadedmetadata', () => {
+      this.duration = video.duration;
+    })
+  }
+
+
+  setTime(event : any) {
+    const video = this.video.nativeElement;
+    video.currentTime = this.currentTime;
+  }
+
+  getTimeRemaining() { 
+    const time = this.duration - this.currentTime;
+    const hours : number = Math.floor(time / 3600);
+    const minutes : number = Math.floor((time % 3600) / 60);
+    const seconds : number = Math.floor(time % 60);
+    return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}`
+    
+  }
+
+  pad(time: number) : string {
+    return time < 10 ? `0${time}` : `${time}`;
+  }
 
 }
