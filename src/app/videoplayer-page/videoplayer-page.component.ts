4f13fbaa0dab250710dbc90ae21fa7dab4205ece
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-10-17 09:09:47 +0200

    adding video.js

--- src/app/videoplayer-page/videoplayer-page.component.ts
+++ src/app/videoplayer-page/videoplayer-page.component.ts
@@ -1,90 +1,91 @@
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { VjsPlayerComponent } from './vjs-player/vjs-player.component';
 
 @Component({
   selector: 'app-videoplayer-page',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, VjsPlayerComponent],
   templateUrl: './videoplayer-page.component.html',
   styleUrl: './videoplayer-page.component.scss'
 })
 export class VideoplayerPageComponent {
 
-  @ViewChild('video') video! : ElementRef<HTMLVideoElement>;
-  @ViewChild('progressbar') progressbar! : ElementRef<HTMLDivElement>;
+  // @ViewChild('video') video! : ElementRef<HTMLVideoElement>;
+  // @ViewChild('progressbar') progressbar! : ElementRef<HTMLDivElement>;
 
-  duration: number = 0;
-  currentTime : number = 0;
-  volume: number = 1;
+  // duration: number = 0;
+  // currentTime : number = 0;
+  // volume: number = 1;
 
-  bufferedPercent: number = 0;
-  playedPercent: number = 0;
+  // bufferedPercent: number = 0;
+  // playedPercent: number = 0;
 
-  isDragging : boolean = false;
+  // isDragging : boolean = false;
 
-  ngAfterViewInit() {
-    const video = this.video.nativeElement;
-    video.volume = this.volume;
-    video.addEventListener('loadedmetadata', () => {
-      this.duration = video.duration;
-    })
-  }
+  // ngAfterViewInit() {
+  //   const video = this.video.nativeElement;
+  //   video.volume = this.volume;
+  //   video.addEventListener('loadedmetadata', () => {
+  //     this.duration = video.duration;
+  //   })
+  // }
 
-  setTime(event : any) {
-    const video = this.video.nativeElement;
-    video.currentTime = this.currentTime;
-  }
+  // setTime(event : any) {
+  //   const video = this.video.nativeElement;
+  //   video.currentTime = this.currentTime;
+  // }
 
-  getTimeRemaining() { 
-    const time = this.duration - this.currentTime;
-    const hours : number = Math.floor(time / 3600);
-    const minutes : number = Math.floor((time % 3600) / 60);
-    const seconds : number = Math.floor(time % 60);
-    return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}`
+  // getTimeRemaining() { 
+  //   const time = this.duration - this.currentTime;
+  //   const hours : number = Math.floor(time / 3600);
+  //   const minutes : number = Math.floor((time % 3600) / 60);
+  //   const seconds : number = Math.floor(time % 60);
+  //   return `${this.pad(hours)}:${this.pad(minutes)}:${this.pad(seconds)}`
     
-  }
+  // }
 
-  pad(time: number) : string {
-    return time < 10 ? `0${time}` : `${time}`;
-  }
+  // pad(time: number) : string {
+  //   return time < 10 ? `0${time}` : `${time}`;
+  // }
 
 
-  // Progress Bar - Drag & Drop
+  // // Progress Bar - Drag & Drop
 
-  onMouseDownThumb(event : MouseEvent) : void {
-    event.preventDefault();
-    this.isDragging = true;
-    document.addEventListener('mousemove', this.onMouseMove);
-    document.addEventListener('mouseup', this.onMouseUp);
-    this.thumbDragged(event.clientX);
-  }
+  // onMouseDownThumb(event : MouseEvent) : void {
+  //   event.preventDefault();
+  //   this.isDragging = true;
+  //   document.addEventListener('mousemove', this.onMouseMove);
+  //   document.addEventListener('mouseup', this.onMouseUp);
+  //   this.thumbDragged(event.clientX);
+  // }
 
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove = (event: MouseEvent) => {
-    if (this.isDragging) {
-      this.thumbDragged(event.clientX);
-    }
-  }
+  // @HostListener('document:mousemove', ['$event'])
+  // onMouseMove = (event: MouseEvent) => {
+  //   if (this.isDragging) {
+  //     this.thumbDragged(event.clientX);
+  //   }
+  // }
 
-  @HostListener('document:mouseup', ['$event'])
-  onMouseUp = (event: MouseEvent) => {
-    if (this.isDragging) {
-      this.isDragging = false;
-      this.thumbDragged(event.clientX);
-    }
-  }
+  // @HostListener('document:mouseup', ['$event'])
+  // onMouseUp = (event: MouseEvent) => {
+  //   if (this.isDragging) {
+  //     this.isDragging = false;
+  //     this.thumbDragged(event.clientX);
+  //   }
+  // }
 
-  thumbDragged(clientX: number): void {
-    const progressbar = this.progressbar.nativeElement;
-    const rect = progressbar.getBoundingClientRect();
-    let x = clientX - rect.left;
-    x = Math.max(0, Math.min(x, rect.width)); // Begrenzung von 0 bis zur Breite des Containers
-    const percent = x / rect.width;
-    this.playedPercent = percent * 100;
-    const video: HTMLVideoElement = this.video.nativeElement;
-    this.currentTime = percent * this.duration;
+  // thumbDragged(clientX: number): void {
+  //   const progressbar = this.progressbar.nativeElement;
+  //   const rect = progressbar.getBoundingClientRect();
+  //   let x = clientX - rect.left;
+  //   x = Math.max(0, Math.min(x, rect.width)); // Begrenzung von 0 bis zur Breite des Containers
+  //   const percent = x / rect.width;
+  //   this.playedPercent = percent * 100;
+  //   const video: HTMLVideoElement = this.video.nativeElement;
+  //   this.currentTime = percent * this.duration;
 
-  }
+  // }
 
 
 }
---
+++ src/app/videoplayer-page/vjs-player/vjs-player.component.ts
@@ -0,0 +1,148 @@
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import videojs from 'video.js';
+import Player from 'video.js/dist/types/player';
+
+@Component({
+  selector: 'app-vjs-player',
+  standalone: true,
+  imports: [],
+  templateUrl: './vjs-player.component.html',
+  styleUrl: './vjs-player.component.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class VjsPlayerComponent {
+  @ViewChild('target', { static: true }) target!: ElementRef;
+
+  // See options: https://videojs.com/guides/options
+  @Input() options!: {
+    fluid: boolean;
+    aspectRatio: string;
+    autoplay: boolean;
+    controls: boolean;
+    controlBar: {
+      subsCapsButton: boolean;
+      playbackRateMenuButton: boolean;
+
+      durationDisplay: boolean;
+      progressControl: boolean;
+      remainingTimeDisplay: boolean;
+      currentTimeDisplay: boolean;
+      timeDivider: boolean;
+
+      pictureInPictureToggle: boolean;
+    };
+    sources: {
+      src: string;
+      type: string;
+    }[];
+    tracks: { src: string; kind: string; srclang: string; label: string }[];
+    playbackRates: number[];
+  };
+
+  player!: Player;
+
+  constructor(private elementRef: ElementRef) {}
+
+  // Instantiate a Video.js player OnInit
+  ngOnInit() {
+    this.player = videojs(
+      this.target.nativeElement,
+      this.options,
+      function onPlayerReady() {
+        console.log('onPlayerReady', this);
+      }
+    );
+    this.addCustomButtons();
+    this. addVideoTitle();
+  }
+
+  // Dispose the player OnDestroy
+  ngOnDestroy() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+
+  addCustomButtons() {
+    this.addRewindButton();
+    this.addForwardButton();
+  }
+
+  addRewindButton() {
+    const Button = videojs.getComponent('Button');
+
+    class RewindButton extends Button {
+      constructor(player: Player, options?: any) {
+        super(player, options);
+        this.addClass('vjs-rewind-button');
+      }
+
+      handleClick() {
+        const currentTime = this.player().currentTime();
+        if (currentTime) this.player().currentTime(currentTime - 10); // Rewind 10 seconds
+      }
+    }
+
+    // register button in video.js
+    videojs.registerComponent('RewindButton', RewindButton);
+
+    // add button to control bar
+    this.player.ready(() => {
+      this.player.getChild('controlBar')!.addChild('RewindButton', {controlText: '10 seconds back'}, 1);
+    });
+  }
+
+
+  addForwardButton() {
+    const Button = videojs.getComponent('Button');
+
+    class ForwardButton extends Button {
+      constructor(player: Player, options?: any) {
+        super(player, options);
+        this.addClass('vjs-forward-button');
+      }
+
+      handleClick() {
+        const currentTime = this.player().currentTime() || 0;
+        this.player().currentTime(currentTime + 10); // Forward 10 seconds
+      }
+    }
+
+    // register button in video.js
+    videojs.registerComponent('ForwardButton', ForwardButton);
+
+    // add button to control bar
+    this.player.ready(() => {
+      this.player.getChild('controlBar')!.addChild('ForwardButton', {controlText: '10 seconds forward'}, 2);
+    });
+  }
+
+
+  addVideoTitle() {
+    const Component = videojs.getComponent('Component')
+
+    class VideoTitle extends Component {
+      constructor(player: Player, options: any) {
+        super(player, options);
+        this.addClass('vjs-video-title');
+        this.el_.innerHTML = options.title || 'Untitled Video';
+      }
+    }
+
+    videojs.registerComponent('VideoTitle', VideoTitle);
+
+    this.player.ready(() => {
+      const videoTitle = 'Mein Videotitel';
+      this.player.getChild('controlBar')!.addChild('VideoTitle',{ title: videoTitle}, 6);
+    });
+  }
+}
