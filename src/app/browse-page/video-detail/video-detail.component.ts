db34163e8fa7e26cebb9095d5298e03e1cf82e55
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2024-11-07 09:32:53 +0100

    update video detail

--- src/app/browse-page/video-detail/video-detail.component.ts
+++ src/app/browse-page/video-detail/video-detail.component.ts
@@ -4,40 +4,72 @@ import { Subscription } from 'rxjs';
 import { ContentService } from '../../services/content/content.service';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { BreakpointService } from '../../services/breakpoint/breakpoint.service';
+import { Video } from '../../shared/interfaces/video';
 
 @Component({
   selector: 'app-video-detail',
   standalone: true,
   imports: [HeaderComponent],
   templateUrl: './video-detail.component.html',
-  styleUrl: './video-detail.component.scss'
+  styleUrl: './video-detail.component.scss',
 })
 export class VideoDetailComponent {
-
   videoId: string | null = null;
   private routeSub!: Subscription;
-  video: any;
+  video: Video = {
+    id: 0,
+    created_at: '',
+    title: '',
+    description: '',
+    genres: [],
+    thumbnail_url: '',
+    video_url: '',
+  };
 
   isMobile: boolean = false;
 
-  constructor(private route: ActivatedRoute, private router: Router, private contentService: ContentService, private breakpointService: BreakpointService) {}
+  /**
+   * Creates an instance of VideoDetailComponent.
+   *
+   * @param {ActivatedRoute} route - Activated route for accessing query parameters.
+   * @param {Router} router - Router for navigation.
+   * @param {ContentService} contentService - Service for retrieving video details.
+   * @param {BreakpointService} breakpointService - Service for detecting screen size changes and responsive adjustments.
+   */
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private contentService: ContentService,
+    private breakpointService: BreakpointService
+  ) {}
 
+  /**
+   * Angular lifecycle hook that runs after the component is initialized.
+   * Subscribes to query parameters for video ID and loads video details.
+   * Also detects if the layout should adjust for mobile devices.
+   */
   ngOnInit(): void {
-    this.routeSub = this.route.queryParamMap.subscribe(params => {
+    this.routeSub = this.route.queryParamMap.subscribe((params) => {
       this.videoId = params.get('vid');
       this.loadVideo();
-    })
+    });
 
-    this.breakpointService.isMobile().subscribe(result => {
+    this.breakpointService.isMobile().subscribe((result) => {
       this.isMobile = result.matches;
-    })
+    });
   }
 
-
-  async loadVideo() {
+  /**
+   * Loads video details from the content service based on the video ID.
+   * Updates the `video` property with the retrieved data.
+   *
+   * @async
+   * @returns {Promise<void>}
+   */
+  async loadVideo(): Promise<void> {
     if (this.videoId) {
       try {
-        const response : any = await this.contentService.getVideo(this.videoId);
+        const response: any = await this.contentService.getVideo(this.videoId);
         console.log(response);
         this.video = response || {};
       } catch (error) {
@@ -46,15 +78,27 @@ export class VideoDetailComponent {
     }
   }
 
+  /**
+   * Closes the video detail view and navigates back to the browse page.
+   */
   closeModal() {
     this.router.navigate(['/browse']);
   }
 
+  /**
+   * Angular lifecycle hook that runs when the component is destroyed.
+   * Unsubscribes from route parameter updates.
+   */
   ngOnDestroy(): void {
     this.routeSub.unsubscribe();
   }
 
-  playVideo(id: string) {
-    this.router.navigate([`/watch/${id}`])
+  /**
+   * Navigates to the video watch page based on the provided video ID.
+   *
+   * @param {number} id - The ID of the video to watch.
+   */
+  watchVideo(id: number) {
+    this.router.navigate([`/watch/${id}`]);
   }
 }
